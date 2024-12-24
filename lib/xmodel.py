7b858759cc35bb2473d9e78e086342fec7d48790
宣源 <xuanyuan.lb@alibaba-inc.com>
2024-12-24 17:50:54 +0800

    add cache

--- lib/xmodel.py
+++ lib/xmodel.py
@@ -9,6 +9,8 @@ def download_hg_model(model_id:str,exDir:str=''):
     model_checkpoint = os.path.join(folder_paths.models_dir, exDir, os.path.basename(model_id))
     print(model_checkpoint)
     if not os.path.exists(model_checkpoint):
+        if os.path.exists("/stable-diffusion-cache/models/Leffa"):
+            return "/stable-diffusion-cache/models/Leffa"
         from huggingface_hub import snapshot_download
         snapshot_download(repo_id=model_id, local_dir=model_checkpoint, local_dir_use_symlinks=False)
     return model_checkpoint
